28343ff6c3776f95438426d6a0fab18ba80bfea2
ControlCaret <controlcaret@gmail.com>
2020-09-22 21:19:00 +0900

    Add ox check

--- execute.py
+++ execute.py
@@ -3,14 +3,26 @@ from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template(
-        'ox.html', 
-        displayTitle = "OX Quiz Example Template", 
-        displayText = "displayText example", 
-        displayProblem = "displayProblem example"
-    )
+def index():
+	return render_template('index.html')
+
+@app.route('/oxexample')
+def oxexample():
+	return render_template(
+		'ox.html', 
+		displayTitle = "OX Quiz Example Template", 
+		displayText = "displayText example", 
+		displayProblem = "displayProblem example",
+		quizNumber = 0
+	)
+
+@app.route('/check', methods=['POST'])
+def quizCheck():
+	answer = request.form['ox']
+	quizNumber = request.form['quizNumber']
+	return answer + ' ' + quizNumber
+
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+	app.debug = True
+	app.run()
